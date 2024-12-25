c29cddf33abcab048bfea851c2eca08a1fdb2b3b
DipaliPatil2713 <dipalipatil2713@gmail.com>
2024-12-25 13:53:19 +0530

    mail project

---
+++ src/main/java/com/fullstack/SpringbootmailexApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmailexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmailexApplication.class, args);
+	}
+
+}
